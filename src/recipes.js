95b33873166fd4a1cedbf6934b5e86d8b1f4c011
Geoffrey Gao <geoffreygao1@gmail.com>
2023-01-16 13:55:23 +0900

    Updated fetch API request to pull app_id from .env

--- src/recipes.js
+++ src/recipes.js
@@ -1,8 +1,7 @@
 export default class RecipeFinder {
   static async getRecipes(food, mealType) {
     try {
-  
-      const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${food}&app_id=d760aafd&app_key=${process.env.API_KEY}&mealType=${mealType}&random=true`);
+      const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${food}&app_id=${process.env.app_id}&app_key=${process.env.app_key}&mealType=${mealType}&random=true`);
       //(`https://api.edamam.com/api/recipes/v2?type=public&q=${food}&app_id=d760aafd&app_key=fb7afac050e24a5419954376cafa27ad&mealType=${mealType}&random=true`);
       const jsonResponse = await response.json();
       if (!response.ok) {
@@ -11,8 +10,8 @@ export default class RecipeFinder {
       }
       // console.log(response);
       // console.log(jsonResponse);
-      return jsonResponse; 
-    } catch(error) {
+      return jsonResponse;
+    } catch (error) {
       return error;
     }
   }
