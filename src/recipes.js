7e7f1dd4393b119ad3ff0a3edf17307bf8a49e16
Richelle <richelle.ak@gmail.com>
2023-01-17 15:03:39 -0800

    add get URL function to account for blank user input fields

--- src/recipes.js
+++ src/recipes.js
@@ -1,8 +1,8 @@
 export default class RecipeFinder {
-    static async getRecipes(food, mealType) {
+    static async getRecipes(newUrl) {
       try {
-        const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${food}&app_id=d760aafd&app_key=${process.env.API_KEY}&mealType=${mealType}&random=true`);
-        //(`https://api.edamam.com/api/recipes/v2?type=public&q=${food}&app_id=d760aafd&app_key=fb7afac050e24a5419954376cafa27ad&mealType=${mealType}&random=true`);
+        const response = await fetch(newUrl);
+
         const jsonResponse = await response.json();
         if (!response.ok) {
           const errorMessage = `${response.status} ${response.statusText} ${jsonResponse.message}`;
