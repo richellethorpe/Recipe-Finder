b6005158b4e1a1f5333dcb3db434066981ac6972
Geoffrey Gao <geoffreygao1@gmail.com>
2023-01-26 11:21:26 +0900

    Added tags to default recipes

--- src/services/default_recipes.js
+++ src/services/default_recipes.js
@@ -5,7 +5,7 @@ export const defaultRecipes = [
       "image": "https://minimalistbaker.com/wp-content/uploads/2022/11/Sweet-Potato-Blondies-8-1024x1536.jpg",
       "url": "https://minimalistbaker.com/tahini-sweet-potato-blondies/",
       "label": "Tahini Sweet Potato Blondies",
-      "healthLabels": ['Vegetarian', 'Vegan']
+      "healthLabels": ['Vegetarian', 'Vegan', 'Gluten-Free', 'Dairy-Free']
     }
   },
   {
@@ -13,7 +13,7 @@ export const defaultRecipes = [
       "image": "https://minimalistbaker.com/wp-content/uploads/2022/11/Smoky-Black-Bean-Quinoa-Chili-Bowl-6-1024x1536.jpg",
       "url": "https://minimalistbaker.com/smoky-vegan-black-bean-quinoa-chili-bowl/",
       "label": "Smoky Black Bean & Quinoa Chili Bowl",
-      "healthLabels": ['Vegetarian', 'Vegan']
+      "healthLabels": ['Vegetarian', 'Vegan', 'Gluten-Free', 'Dairy-Free']
     }
   },
   {
@@ -21,7 +21,7 @@ export const defaultRecipes = [
       "image": "https://minimalistbaker.com/wp-content/uploads/2022/11/Black-bean-chili-6-1024x1536.jpg",
       "url": "https://minimalistbaker.com/1-pot-chipotle-black-bean-chili/",
       "label": "1-Pot Chipotle Black Bean Chili",
-      "healthLabels": ['Vegetarian', 'Vegan']
+      "healthLabels": ['Vegetarian', 'Vegan', 'Gluten-Free', 'Dairy-Free']
     }
   },
   {
@@ -29,7 +29,7 @@ export const defaultRecipes = [
       "image": "https://minimalistbaker.com/wp-content/uploads/2022/11/Creamy-jalapeno-sauce-6-1024x1536.jpg",
       "url": "https://minimalistbaker.com/creamy-cashew-jalapeno-sauce/",
       "label": "Creamy Cashew Jalapeno Sauce",
-      "healthLabels": ['Vegetarian', 'Vegan']
+      "healthLabels": ['Vegetarian', 'Vegan', 'Gluten-Free', 'Dairy-Free']
     }
   },
   {
@@ -37,7 +37,7 @@ export const defaultRecipes = [
       "image": "https://minimalistbaker.com/wp-content/uploads/2022/07/Fire-Cider-Shots-in-just-15-minutes-Spicy-sweet-and-FULL-of-health-benefits-minimalistbaker-recipe-firecider-shots-5-1024x1536.jpg",
       "url": "https://minimalistbaker.com/immune-boosting-fresh-ginger-recipes/",
       "label": "Immune-Boosting Fresh Ginger Recipes",
-      "healthLabels": []
+      "healthLabels": ['Gluten-Free', 'Dairy-Free']
     }
   },
   {
@@ -45,7 +45,7 @@ export const defaultRecipes = [
       "image": "https://minimalistbaker.com/wp-content/uploads/2022/11/Vegan-Chai-Spiced-Creme-Brulee-7-1024x1536.jpg",
       "url": "https://minimalistbaker.com/vegan-chai-spiced-creme-brulee/",
       "label": "Vegan Chai-Spiced Creme Brulee",
-      "healthLabels": ['Vegetarian', 'Vegan']
+      "healthLabels": ['Vegetarian', 'Vegan', 'Gluten-Free', 'Dairy-Free']
     }
   },
   {
@@ -53,7 +53,7 @@ export const defaultRecipes = [
       "image": "https://minimalistbaker.com/wp-content/uploads/2022/11/Vegan-Lentil-Bolognese-8-1024x1536.jpg",
       "url": "https://minimalistbaker.com/vegan-bolognese-with-mushrooms-red-lentils/",
       "label": "Vegan Bolognese with Mushrooms & Red Lentils",
-      "healthLabels": ['Vegetarian', 'Vegan']
+      "healthLabels": ['Vegetarian', 'Vegan', 'Gluten-Free', 'Dairy-Free']
     }
   },
   {
@@ -61,7 +61,7 @@ export const defaultRecipes = [
       "image": "https://minimalistbaker.com/wp-content/uploads/2022/10/Kale-Salad-with-Crispy-Quinoa-6-683x1024.jpg",
       "url": "https://minimalistbaker.com/crispy-quinoa-roasted-vegetable-kale-salad/",
       "label": "Crispy Quinoa & Roasted Vegetable Kale Salad",
-      "healthLabels": ['Vegetarian', 'Vegan']
+      "healthLabels": ['Vegetarian', 'Vegan', 'Gluten-Free', 'Dairy-Free']
     }
   },
   {
@@ -69,7 +69,7 @@ export const defaultRecipes = [
       "image": "https://minimalistbaker.com/wp-content/uploads/2022/10/GLUTEN-FREE-Biscotti-with-Almonds-and-Chocolate-Vegan-perfectly-crunchy-10-ingredients-minimalistbaker-recipe-glutenfree-almond-biscotti-9-683x1024.jpg",
       "url": "https://minimalistbaker.com/gluten-free-chocolate-almond-biscotti/",
       "label": "Gluten-Free Chocolate Almond Biscotti",
-      "healthLabels": ['Vegetarian']
+      "healthLabels": ['Vegetarian', 'Gluten-Free']
     }
   }
 ];
